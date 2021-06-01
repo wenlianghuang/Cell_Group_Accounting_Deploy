e7e7e520b2becdae3e365f550e051706c085ce83
wenlianghuang <wenlianghuang@gmail.com>
2021-06-01 18:27:05 +0800

    20210601 modified file

--- src/components/login_user.js
+++ src/components/login_user.js
@@ -50,7 +50,7 @@ export default function LogInUser(){
             setPassword('')
         })
         .catch((error)=>{
-            console.log("error")
+            console.log(error)
         })
     }
     return (
