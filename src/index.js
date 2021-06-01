37f26bc2fb2687a1df4ec5303a74f2adc8cfe5bf
wenlianghuang <wenlianghuang@gmail.com>
2021-06-01 18:54:28 +0800

    20210601 modified file

--- src/index.js
+++ src/index.js
@@ -13,10 +13,39 @@ import { WebSocketLink } from 'apollo-link-ws';
 import { getMainDefinition } from 'apollo-utilities';
 
 
+const httpLink = new HttpLink({
+  uri: 'https://main.dslriwa6h76ii.amplifyapp.com'
+})
 
+const wsLink = new WebSocketLink({
+  uri: `https://main.dslriwa6h76ii.amplifyapp.com`,
+  options: {reconnect: true}
+})
 
+const link = split(
+  ({query}) => {
+    const definition = getMainDefinition(query)
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    )
+  },
+  wsLink,
+  httpLink
+)
+
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache().restore({})
+}
+)
+const wrapperApollo = (
+  <ApolloProvider client={client}>
+    <App/>
+  </ApolloProvider>
+)
 ReactDOM.render(
-  <App/>,
+  wrapperApollo ,
   document.getElementById('root')
 );
 
