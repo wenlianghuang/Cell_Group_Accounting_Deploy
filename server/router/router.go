ab76ebdd47a656ccbcfc0c2365415ec17aeb6089
wenlianghuang <wenlianghuang@gmail.com>
2021-06-03 16:49:19 +0800

    20210602 modified file

--- server/router/router.go
+++ server/router/router.go
@@ -3,12 +3,18 @@ package router
 import (
 	"backend/middleware"
 
+	"net/http"
+
 	"github.com/gorilla/mux"
 )
 
 func Router() *mux.Router {
 
 	router := mux.NewRouter()
+	buildHandler := http.FileServer(http.Dir("/Users/Apple/GoogleMatt/Web Programming/Cell_Group_Accounting_Deploy/build"))
+	router.PathPrefix("/").Handler(buildHandler)
+	staticHandler := http.StripPrefix("/static/", http.FileServer(http.Dir("/Users/Apple/GoogleMatt/Web Programming/Cell_Group_Accounting_Deploy/build/static>")))
+	router.PathPrefix("/static/").Handler(staticHandler)
 
 	router.HandleFunc("/api/accounting", middleware.GetAllAccount).Methods("GET", "OPTIONS")
 	router.HandleFunc("/api/accounting", middleware.CreateAccount).Methods("POST", "OPTIONS")
@@ -17,11 +23,5 @@ func Router() *mux.Router {
 	router.HandleFunc("/api/deleteAccount/{id}", middleware.DeleteOneAccount).Methods("DELETE", "OPTIONS")
 	router.HandleFunc("/api/accounting/{id}", middleware.GetOneAccount).Methods("GET", "OPTIONS")
 	router.HandleFunc("/api/deleteCount/{id}", middleware.DeleteOneContent).Methods("PUT", "OPTIONS")
-	//router.HandleFunc("/api/accounting/{id}", middleware.GetOneAccount).Methods("GET", "OPTIONS")
-	//router.HandleFunc("/api/task/{id}", middleware.TaskComplete).Methods("PUT", "OPTIONS")
-	//router.HandleFunc("/api/undoTask/{id}", middleware.TaskUndo).Methods("PUT", "OPTIONS")
-	//router.HandleFunc("/api/deleteTask/{id}", middleware.DeleteTask).Methods("DELETE", "OPTIONS")
-	//router.HandleFunc("/api/deleteAllTask", middleware.DeleteAllTask).Methods("DELETE", "OPTIONS")
-	//router.HandleFunc("/api/addtask/{id}", middleware.AddSubTask).Methods("PUT", "OPTIONS")
 	return router
 }
