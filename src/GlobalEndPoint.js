e03183f0f783614981282288b465de22547ea149
wenlianghuang <wenlianghuang@gmail.com>
2021-06-04 18:17:40 +0800

    20210604 modified file

--- src/GlobalEndPoint.js
+++ src/GlobalEndPoint.js
@@ -1,4 +1,4 @@
-export const Endpoint = "http://localhost:9080"
+//export const Endpoint = "http://localhost:9080"
 //export const Endpoint = "https://cell-group-accounting.herokuapp.com:9080"
-//export const Endpoint = "https://main.dslriwa6h76ii.amplifyapp.com:9080"
+export const Endpoint = "https://main.dslriwa6h76ii.amplifyapp.com:9080"
 //export const Endpoint = ""
